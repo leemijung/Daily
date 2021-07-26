e2f00e94691599a2d3825e81a51aa7dd636ce1b0
leemijung <kongs5056@naver.com>
2021-07-26 16:30:28 +0900

    0726 calender_main

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.example.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.util.*
 
-//, BottomNavigationView.OnNavigationItemSelectedListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //bottom_navigationview.setOnNavigationItemSelectedListener(this)
 
-        val CalendarView = findViewById<CalendarView>(R.id.calendarView)
+        val calendarView :MaterialCalendarView=findViewById(R.id.main_calendarView)
 
-        CalendarView?.setOnDateChangeListener {view, year, month, dayOfMonth ->
-            val msg:String = year.toString() +"/"+ month.toString() +"/"+ dayOfMonth.toString()
-            runOnUiThread {
-                Toast.makeText(this,msg,Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
-            }
-        }
+        //var startTimeCalendar = Calendar.getInstance()
+        //var endTimeCalendar = Calendar.getInstance()
+
+        //val currentYear = startTimeCalendar.get(Calendar.YEAR)
+        //val currentMonth = startTimeCalendar.get(Calendar.MONTH)
+        //val currentDate = startTimeCalendar.get(Calendar.DATE)
+
+        //endTimeCalendar.set(Calendar.MONTH, currentMonth + 3)
+
+        //calendarView.state().edit()
+        //        .setFirstDayOfWeek(Calendar.SUNDAY)
+        //        .setMinimumDate(CalendarDay.from(currentYear, currentMonth, 1))
+        //        .setMaximumDate(CalendarDay.from(currentYear, currentMonth + 3, endTimeCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)))
+        //        .setCalendarDisplayMode(CalendarMode.MONTHS)
+        //        .commit()
+
+
+        //토-파랑, 일-빨강   캘린더 색상변경
+        val saturdayDecorator = SaturdayDecorator()
+        val sundayDecorator = SundayDecorator()
+        calendarView.addDecorators(saturdayDecorator, sundayDecorator)
+
+
+
     }
 }
